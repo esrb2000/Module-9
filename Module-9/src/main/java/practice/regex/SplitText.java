0b35814431d6ecbbc26538b164d60e84265a155c
Boris Kuptsevich <esrb2000@bk.ru>
2023-02-07 20:52:52 +0700

    Split text is done

--- Module-9/src/main/java/practice/regex/SplitText.java
+++ Module-9/src/main/java/practice/regex/SplitText.java
@@ -10,7 +10,7 @@ public class SplitText {
     String clearedText = text.strip();
     String regex = "[0-9.,:;]";
     clearedText = clearedText.replaceAll(regex, "");
-    String [] words = clearedText.split("\s+");
+    String [] words = clearedText.split("\\s+");
     String splitText = "";
     for (int i = 0; i < words.length - 1; i++) {
       words[i] = words[i] + '\n';
