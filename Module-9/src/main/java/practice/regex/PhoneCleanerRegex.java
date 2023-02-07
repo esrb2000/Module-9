8c3396facbfcc5aafb67fd1a39a8c064216aba85
Boris Kuptsevich <esrb2000@bk.ru>
2023-02-07 22:46:20 +0700

    Phone cleaner is process

--- Module-9/src/main/java/practice/regex/PhoneCleanerRegex.java
+++ Module-9/src/main/java/practice/regex/PhoneCleanerRegex.java
@@ -13,8 +13,27 @@ public class PhoneCleanerRegex {
         scanner.close();
         break;
       }
-
-      // TODO:напишите ваш код тут, результат вывести в консоль.
+      String regex = "[^0-9]";
+      input = input.replaceAll(regex, "");
+      String [] number = input.split("");
+      if ((number.length > 11 && number.length < 10) && ((!number[0].equals("7")) && (!number[0].equals("8")))) {
+        System.out.println("Неверный формат номера");
+      } else if (number.length == 10) {
+        String outputNumber = "7";
+        for ( int i = 0; i < number.length; i++){
+          outputNumber = outputNumber + number[i];
+        }
+        System.out.println(outputNumber);
+      } else if (number[0].equals("8")) {
+        String outputNumber = "7";
+        for (int i = 1; i < number.length; i++) {
+          outputNumber = outputNumber + number[i];
+        }
+        System.out.println(outputNumber);
+      } else {
+        System.out.println(number);
+      }
+      //System.out.println(number.length > 11 && number.length < 10 && !number[0].equals("7") && !number[0].equals("8"));
     }
   }
 
